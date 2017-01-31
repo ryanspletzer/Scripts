86608f241774436cdfa77f3f0f483a71b45bef1d
Spletzer, Ryan (RS) <RSSpletzer@dow.com>
2017-01-31 13:59:21 -0500

    Copying 2016 Lability update generation driver script

---
+++ PowerShell/Configurations/Lability2016StandardEvalUpdateGeneration/TestLabGuide.ps1
@@ -0,0 +1,28 @@
+Configuration TestLabGuide {
+<#
+    Requires the following custom DSC resources:
+        xComputerManagement (v1.4.0.0 or later): https://github.com/PowerShell/xComputerManagement
+#>
+    param ()
+    Import-DscResource -Module xComputerManagement, xNetworking
+
+    node $AllNodes.Where({$true}).NodeName {
+        LocalConfigurationManager {
+            RebootNodeIfNeeded   = $true;
+            AllowModuleOverwrite = $true;
+            ConfigurationMode = 'ApplyOnly';
+            CertificateID = $node.Thumbprint;
+        }
+
+        xComputer 'Hostname' {
+            Name = $node.NodeName;
+        }
+
+        xNetConnectionProfile 'EthernetPrivate' {
+            InterfaceAlias = 'Ethernet'
+            NetworkCategory = 'Private'
+            IPv4Connectivity = 'Internet'
+            IPv6Connectivity = 'NoTraffic'
+        }
+    }
+}
