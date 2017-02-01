cf862e6b30500670b826068300b6ec3d2ae7395e
Ryan Spletzer <ryanspletzer@gmail.com>
2017-01-31 21:37:14 -0500

    Adding Win10 LTSB (1511) update gen config

---
+++ PowerShell/Configurations/LabilityWIN10x64LTSBStandardEvalUpdateGeneration/LabilityTestLabGuide.ps1
@@ -0,0 +1,44 @@
+Import-Module Lability -Force
+Get-Command -Module Lability
+Get-LabHostDefault
+Set-LabHostDefault -ConfigurationPath D:\TestLab\Configurations -DifferencingVhdPath 'D:\TestLab\VM Disks' -HotfixPath D:\TestLab\Hotfixes -IsoPath D:\TestLab\ISOs -ParentVhdPath 'C:\TestLab\Parent Disks' -ResourcePath D:\TestLab\Resources
+
+<#
+    Test host configuration and start configuration if necessary (Start-LabConfiguration calls Test-LabConfiguration anyway!) #>
+Test-LabHostConfiguration -Verbose
+Start-LabHostConfiguration -Verbose
+
+<#
+    Import configuration into session and generate the MOFs #>
+. .\TestLabGuide.ps1
+TestLabGuide -OutputPath D:\TestLab\Configurations -ConfigurationData .\TestLabGuide.psd1
+
+<#
+    Set the lab VM defaults, create the lab and start the VMs #>
+Get-LabVMDefault
+Set-LabVMDefault -SystemLocale en-US -InputLocale 0409:00000409 -UserLocale en-US -RegisteredOrganization 'Contoso' -StartupMemory 4GB
+Start-LabConfiguration -ConfigurationData .\TestLabGuide.psd1 -Path D:\TestLab\Configurations -Verbose -Force
+
+## ADD ADDITIONAL NIC
+#Add-VMNetworkAdapter -VMName EDGE1 -SwitchName Internet
+
+<# Start Lab #>
+Start-Lab -ConfigurationData .\TestLabGuide.psd1 -Verbose
+
+<#
+    List available 'included' media #>
+Get-LabMedia | Select Id, Description, Filename
+
+<#
+    List (currently) configured images #>
+Get-LabImage | Select Id, ImagePath
+
+<# Stop Lab #>
+Stop-Lab -ConfigurationData .\TestLabGuide.psd1 -Verbose
+
+
+
+
+<# Reset lab #>
+Import-Module Lability -Force
+Remove-LabConfiguration -ConfigurationData .\TestLabGuide.psd1 -Verbose
