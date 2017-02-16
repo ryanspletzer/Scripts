6b99f59f6e4cbac0be75b3a2fb7dac04ac4d07fd
Ryan Spletzer <ryanspletzer@gmail.com>
2017-02-15 21:47:24 -0500

    jekyll setup script

---
+++ Bash/JekyllSetupUbuntu.sh
@@ -0,0 +1,15 @@
+currentUser=$USER
+sudo -s
+apt update
+apt upgrade
+apt-get install build-essential
+apt-get install zlib1g-dev
+apt-add-repository ppa:brightbox/ruby-ng
+apt update
+apt install ruby2.3 ruby2.3-dev ruby-switch
+ruby -v
+ruby-switch --set ruby2.3
+gem install execjs
+gem install jekyll
+sudo -u $currentUser -s
+exit
