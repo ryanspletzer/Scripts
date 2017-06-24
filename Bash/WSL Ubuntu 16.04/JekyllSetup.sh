03c0dd9112512a97b38253c77ee64027ee665fb9
Ryan Spletzer <ryanspletzer@gmail.com>
2017-06-23 23:01:35 -0400

    Update JekyllSetup.sh removing pygments
    Pygments doesn't work on GitHub pages.

--- Bash/WSL Ubuntu 16.04/JekyllSetup.sh
+++ Bash/WSL Ubuntu 16.04/JekyllSetup.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install ruby ruby-dev make gcc
 sudo gem install jekyll bundler
-sudo apt-get install -y python3-pip
-pip3 install --upgrade pip
-pip3 install pygments
-sudo gem install pygments.rb
+# sudo apt-get install -y python3-pip
+# pip3 install --upgrade pip
+# pip3 install pygments
+# sudo gem install pygments.rb
