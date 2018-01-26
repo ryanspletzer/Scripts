0e38da7a92a1433dfbdf2e66f3fe76cc6d7ad81d
Ryan Spletzer <ryanspletzer@gmail.com>
2018-01-26 13:28:48 -0500

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -100,3 +100,17 @@ function Enter-ElevatedPSSession {
 }
 
 New-Alias -Name su -Value Enter-ElevatedPSSession
+
+function Invoke-NetView {
+    [CmdletBinding()]
+    [OutputType([string[]])]
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]
+        $Path
+    )
+    begin { }
+    process {
+        [string[]](net view $Path /all | select -Skip 7 | ?{$_ -match 'disk*'} | %{$_ -match '^(.+?)\s+Disk*'|out-null;$matches[1]})
+    }
+}
