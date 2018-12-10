ee1b46f10a14c21eff92b7c5f7a3de7e21233f7b
Ryan Spletzer <ryan.spletzer@autodesk.com>
2018-12-10 18:34:35 -0500

    Updating setup.sh

--- src/macOS/setup.sh
+++ src/macOS/setup.sh
@@ -4,46 +4,18 @@ mkdir ~/git
 
 defaults write com.apple.Finder AppleShowAllFiles true
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew install awscli
-brew install azure-cli
-brew cask install google-chrome
-brew cask install firefox
 brew install git
 brew cask install java
 brew install git-credential-manager
-brew install bash-git-prompt
+
+brew install bash
+brew install fish
 brew cask install dotnet-sdk
 brew cask install powershell
 brew cask install homebrew/cask-versions/powershell-preview
-brew cask install slack
-brew cask install visual-studio-code
-brew cask install visual-studio
-brew cask install azure-data-studio
-brew cask install postman
-brew cask install citrix-workspace
-brew cask install zoomus
-brew cask install zoom-outlook-plugin
-brew cask install microsoft-azure-storage-explorer
-brew install gnupg
-brew cask install keybase
-brew cask install homebrew/cask-drivers/drobo-dashboard
-brew cask install docker
-brew cask install steam
-brew cask install iterm2
-brew install fish
-brew install ykman
-brew install ykpers
-brew install yubico-piv-tool
-brew cask install yubico-yubikey-manager
-brew cask install yubico-yubikey-personalization-github
-brew cask install yubico-yubikey-piv-manager
-brew cask install microsoft-teams
-brew install bash
-sudo gem install bundler jekyll
+
 sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
 sudo bash -c 'echo /usr/local/bin/pwsh >> /etc/shells'
-chsh -s /usr/local/bin/pwsh
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 omf install agnoster
@@ -60,3 +32,32 @@ sudo pwsh -Command "Install-Module -Name az, AWSPowerShell, posh-git"
 sudo pwsh -Command "Update-Help"
 mkdir ~/.config/powershell
 cp ./.config/powershell/* ~/.config/powershell
+
+brew install awscli
+brew install azure-cli
+brew cask install google-chrome
+brew cask install firefox
+#brew install bash-git-prompt
+brew cask install slack
+brew cask install visual-studio-code
+brew cask install visual-studio
+brew cask install azure-data-studio
+brew cask install postman
+brew cask install citrix-workspace
+brew cask install zoomus
+brew cask install zoom-outlook-plugin
+brew cask install microsoft-azure-storage-explorer
+brew install gnupg
+brew cask install keybase
+brew cask install homebrew/cask-drivers/drobo-dashboard
+brew cask install docker
+brew cask install steam
+brew cask install iterm2
+brew install ykman
+brew install ykpers
+brew install yubico-piv-tool
+brew cask install yubico-yubikey-manager
+brew cask install yubico-yubikey-personalization-github
+brew cask install yubico-yubikey-piv-manager
+brew cask install microsoft-teams
+brew install lastpass-cli
