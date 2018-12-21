6684027e2a1625edd45812f9a4e2c06d1fe93cf1
Ryan Spletzer <ryan.spletzer@autodesk.com>
2018-12-21 10:25:36 -0500

    Updating macOS setup script

--- src/macOS/setup.sh
+++ src/macOS/setup.sh
@@ -28,13 +28,16 @@ cd fonts
 cd ..
 rm -rf fonts
 sudo pwsh -Command "Set-PackageSource -Name PSGallery -Trusted -Force"
-sudo pwsh -Command "Install-Module -Name az, AWSPowerShell, posh-git"
+sudo pwsh -Command "Install-Module -Name Az, AWSPowerShell, posh-git"
 sudo pwsh -Command "Update-Help"
 mkdir ~/.config/powershell
 cp ./.config/powershell/* ~/.config/powershell
 
 brew install awscli
 brew install azure-cli
+brew install packer
+brew install terraform
+brew install vault
 brew cask install google-chrome
 brew cask install firefox
 #brew install bash-git-prompt
